ba645a09833c5b21abf5e7254b42c1d358391d44
Kavita Asija <kavitaasija@Kavitas-MacBook-Pro.local>
2023-10-24 12:08:54 +0530

    [Kavita] remove extra endpoints and remove liveness probe and change the clusterip

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
+import json
+
 from fastapi import FastAPI
 from core.config import settings
 
-app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+
 
 @app.get("/")
 def hello_api():
-    return {"msg":"Hello FastAPI🚀"}
+    return {"msg": "Hello FastAPI🚀"}
